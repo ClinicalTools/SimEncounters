8c1f223cda70df266d1b8394fe6909c03ef38ae5
davisctools <39494394+davisctools@users.noreply.github.com>
2020-02-21 14:50:57 -0500

    Begin implementing Panel functionality to be handled outside the MonoBehaviour
    Also changed "Talk to Patient" to "Talk with Patient"

--- Assets/Scripts/SophReformatting/SimEncounters/Reader/Panels/Types/OrderableGroup/ReaderOrderableItemPanelUI.cs
+++ Assets/Scripts/SophReformatting/SimEncounters/Reader/Panels/Types/OrderableGroup/ReaderOrderableItemPanelUI.cs
@@ -7,6 +7,56 @@ using UnityEngine.UI;
 
 namespace ClinicalTools.SimEncounters.Reader
 {
+    public class ReaderOrderableItemPanelDisplay
+    {
+        protected ReaderScene Reader { get; set; }
+        protected ReaderOrderableItemPanelUI PanelUI { get; set; }
+        protected IValueField[] ValueFields { get; set; }
+
+        public ReaderOrderableItemPanelDisplay(ReaderScene reader, ReaderOrderableItemPanelUI panelUI, KeyValuePair<string, Panel> keyedPanel)
+        {
+            Reader = reader;
+            PanelUI = panelUI;
+            var panel = keyedPanel.Value;
+
+            var valueFieldInitializer = new ReaderValueFieldInitializer(reader);
+            ValueFields = valueFieldInitializer.InitializePanelValueFields(PanelUI.gameObject, panel);
+        }
+
+        public void SetColor(Color color)
+        {
+            color.a = 1;
+            foreach (var image in PanelUI.ColoredImages)
+                image.color = color;
+        }
+
+        public void StartDrag(Vector3 mousePosition)
+        {
+            PanelUI.DragHandle.interactable = false;
+        }
+
+        public void EndDrag(Vector3 mousePosition)
+        {
+            PanelUI.DragHandle.interactable = true;
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            Reader.Mouse.RegisterDraggable(PanelUI);
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            Reader.Mouse.SetCursorState(CursorState.Draggable);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            Reader.Mouse.RemoveCursorState(CursorState.Draggable);
+        }
+
+    }
+
     public class ReaderOrderableItemPanelUI : BaseReaderPanelUI, IDraggable, IPointerDownHandler, IPointerEnterHandler, IPointerExitHandler
     {
         public RectTransform RectTransform => (RectTransform)transform;
