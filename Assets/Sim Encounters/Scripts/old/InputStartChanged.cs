c830225a83468fc4256465a0f5ff9e86f1af5bdc
davisctools <39494394+davisctools@users.noreply.github.com>
2020-11-11 15:55:27 -0500

    Remove more unused assets
    Also reinstated ones that shouldn't have been removed

---
+++ Assets/Sim Encounters/Scripts/old/InputStartChanged.cs
@@ -0,0 +1,17 @@
+﻿using TMPro;
+using UnityEngine;
+
+/// <summary>
+/// Calls the input field's on value changed method at start.
+/// </summary>
+public class InputStartChanged : MonoBehaviour {
+
+	// Use this for initialization
+	void Start()
+    {
+        Debug.LogWarning("InputStartChanged");
+        var input = gameObject.GetComponent<TMP_InputField>();
+        NextFrame.Function(
+            delegate { input.onValueChanged.Invoke(input.text); });
+	}
+}
---
+++ Assets/Sim Encounters/Scripts/old/MenuCase.cs
@@ -0,0 +1,10 @@
+﻿public class MenuCase 
+{
+    public enum CaseType
+    {
+        privateCase = 0,
+        publicCase = 1,
+        privateTemplate = 2,
+        publicTemplate = 3
+    };
+}
